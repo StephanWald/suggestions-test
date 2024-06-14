7311a71e70eae14574ada4d179b9a090256fd351
Stephan Wald <swald@basis.com>
2024-06-14 17:36:00 -0600

    initial commit

--- src/main/java/samples/WebforjHelloWorld.java
+++ src/main/java/samples/WebforjHelloWorld.java
@@ -3,13 +3,18 @@ package samples;
 import static com.webforj.component.optiondialog.OptionDialog.showMessageDialog;
 
 import com.webforj.App;
+import com.webforj.addons.components.suggestionedit.SuggestionEdit;
+import com.webforj.addons.components.suggestionedit.events.ModifiedEvent;
 import com.webforj.annotation.AppTitle;
 import com.webforj.annotation.InlineStyleSheet;
 import com.webforj.component.button.Button;
-import com.webforj.component.button.ButtonTheme;
 import com.webforj.component.html.elements.Paragraph;
+import com.webforj.component.text.Label;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
+
+import java.util.List;
+
 
 /**
  * A simple HelloWorld app.
@@ -26,18 +31,35 @@ import com.webforj.exceptions.WebforjException;
 """)
 @AppTitle("webforJ Hello World")
 public class WebforjHelloWorld extends App {
-  
+  String lastInput = "";
+
+  FakeNames fakeNames = new FakeNames();
   Paragraph hello = new Paragraph("Hello World!");
   Button btn = new Button("Say Hello");
+  private SuggestionEdit se;
+  private Label lbl;
 
   @Override
   public void run() throws WebforjException {
     Frame mainFrame = new Frame();
     mainFrame.addClassName("mainFrame");
 
-    btn.setTheme(ButtonTheme.PRIMARY)
-        .addClickListener(e -> showMessageDialog("Hello World!", "webforJ Message"));
+    App.getPage().addJavaScript("http://localhost:8888/files/dwc-addons/dwc-addons.esm.js",true,"type=module");
 
-    mainFrame.add(hello, btn);
+    lbl = new Label("");
+    mainFrame.add(lbl);
+
+    se = new SuggestionEdit();
+    mainFrame.add(se);
+    se.addModifiedListener(this::onModified);
+ }
+
+  private void onModified(ModifiedEvent modifiedEvent) {
+    String search = modifiedEvent.getDetail();
+    lbl.setText("Search Term: " + search);
+    if (!lastInput.equals(search) && search.length()>2) {
+      final List<String> matching = fakeNames.findMatching(search);
+      se.setSuggestions(matching);
+    }
   }
 }
